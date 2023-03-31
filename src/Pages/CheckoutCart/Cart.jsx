5c2d9e152b5a22737c9dc024712cfc13b16605c5
Nom Phan <np656@drexel.edu>
2023-03-31 14:52:28 -0400

    wip

--- src/Pages/CheckoutCart/Cart.jsx
+++ src/Pages/CheckoutCart/Cart.jsx
@@ -1,22 +1,43 @@
 import CartItem from "./CartItem";
 
 const Cart = ({ cartItems, addToCart, removeFromCart }) => {
+    const dummyData = [{
+        id: 1,
+        title: "Throwback Hip Bag",
+        price: 90,
+        amount: 1,
+        image: "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
+    }]
+    cartItems = dummyData
     const calculateTotal = (items) =>
         items.reduce((acc, item) => acc + item.amount * item.price, 0);
     return (
-        <>
-            <h2>Your Shopping Cart</h2>
-            {cartItems.length === 0 ? <p>No items in cart</p> : null}
-            {cartItems.map((item) => (
-                <CartItem
-                    key={item.id}
-                    item={item}
-                    addToCart={addToCart}
-                    removeFromCart={removeFromCart}
-                />
-            ))}
-            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
-        </>
+        <div class="flex items-start justify-between flex-col h-full">
+            <div>
+                <h2 class="text-lg font-medium text-gray-900 text-center" id="slide-over-title">Shopping cart</h2>
+                {cartItems.length === 0 ? <p class="text-lg font-medium text-gray-900 text-center" id="slide-over-title">No item in cart</p> : null}
+                {cartItems.map((item) => (
+                    <CartItem
+                        key={item.id}
+                        item={item}
+                        addToCart={addToCart}
+                        removeFromCart={removeFromCart}
+                    />
+                ))}
+            </div>
+
+            <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
+                <div class="flex justify-between text-base font-medium text-gray-900">
+                    <p>Total: </p>
+                    <p>${calculateTotal(cartItems).toFixed(2)}</p>
+                </div>
+                <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                <div class="mt-6">
+                    < a
+                        class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/Pages/CheckoutCart/CartItem.jsx
+++ src/Pages/CheckoutCart/CartItem.jsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 
 const CartItem = ({ item, addToCart, removeFromCart }) => (
     <>
-        <div>
+        {/* <div>
             <h3>{item.title}</h3>
             <div className="information">
                 <p>${item.price}</p>
@@ -29,8 +29,32 @@ const CartItem = ({ item, addToCart, removeFromCart }) => (
                     +
                 </Button>
             </div>
-        </div>
-        <img src={item.image} alt={item.title} loading="lazy" />
+        </div> */}
+        <li class="flex py-6">
+            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                <img src={item.image} atl={item.title} class="h-full w-full object-cover object-center" />
+            </div>
+            <div class="ml-4 flex flex-1 flex-col">
+                <div>
+                    <div class="flex justify-between text-base font-medium text-gray-900">
+                        <h3>
+                            <a href="#">{item.title}</a>
+                        </h3>
+                        <p class="ml-4">${(item.amount * item.price).toFixed(2)}</p>
+                    </div>
+                    <p class="mt-1 text-sm text-gray-500">Quantity: {item.amount}</p>
+                </div>
+                <div class="flex flex-1 items-end text-sm">
+                    <div class="flex">
+                        <button type="button">-</button>
+                    </div>
+                    <p class="text-gray-500">{item.amount}</p>
+                    <div class="flex">
+                        <button type="button" >+</button>
+                    </div>
+                </div>
+            </div>
+        </li>
     </>
 );
 
