83bcf34330d5fd20d8ab8aecd244109e46853b2e
Nom Phan <np656@drexel.edu>
2023-04-13 16:46:00 -0400

    resize icon

--- src/Components/SupportChat/SupportChat.jsx
+++ src/Components/SupportChat/SupportChat.jsx
@@ -15,7 +15,7 @@ export default function SupportChat() {
             <button
                 onClick={handleRedirect}
                 type="button" class="fixed bottom-4 right-4 select-none bg-blue-500 hover:bg-blue-700 border-2 text-white text-xl font-bold p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline">
-                <FacebookIcon />
+                <FacebookIcon style={{ fontSize: '36px' }} />
             </button>
         </div>
     );
