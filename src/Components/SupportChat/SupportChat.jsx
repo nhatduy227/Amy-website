bf120c17957ef44f5ccf58656286b8e7c3b094e7
Nom Phan <np656@drexel.edu>
2023-04-13 16:44:21 -0400

    Add support chat

--- src/Components/SupportChat/SupportChat.jsx
+++ src/Components/SupportChat/SupportChat.jsx
@@ -1,50 +1,22 @@
-import React, { useContext, useState } from 'react';
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import React, { useContext } from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import { UserContext } from '../../App';
 
 export default function SupportChat() {
-    const [isChatOpen, setIsChatOpen] = useState(false);
     const user = useContext(UserContext)
-
-    function handleSubmit(user) {
-        console.log(user)
-    }
-
-    const handleToggleChat = () => {
-        setIsChatOpen(!isChatOpen);
+    const handleRedirect = () => {
+        const url = 'https://www.facebook.com/amyfood210';
+        window.open(url, '_blank');
     };
     if (user.role === "admin")
         return null
     return (
         <div className="fixed bottom-4 right-4">
-            {isChatOpen ? (
-                <div className={`flex flex-col bg-white h-96 p-4 rounded-lg shadow-md ${isChatOpen ? '' : 'hidden'}`}>
-                    <div className="flex justify-between mb-4">
-                        <h3 className="font-bold">Customer Support Chat</h3>
-                        <button
-                            className="text-gray-500 hover:text-gray-700 focus:outline-none"
-                            onClick={handleToggleChat}
-                        >
-                            <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M6.707 6.707a1 1 0 0 1 1.414 0L10 8.586l1.879-1.879a1 1 0 1 1 1.414 1.414L11.414 10l1.879 1.879a1 1 0 0 1-1.414 1.414L10 11.414l-1.879 1.879a1 1 0 0 1-1.414-1.414L8.586 10l-1.879-1.879a1 1 0 0 1 0-1.414z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            ) : <button
-                onClick={() => {
-                    handleToggleChat();
-                    handleSubmit(user)
-                }}
-                type="button" class="fixed bottom-4 right-4 select-none bg-primary-default border-2 text-white text-xl font-bold p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline">
-                <ChatBubbleOutlineOutlinedIcon />
+            <button
+                onClick={handleRedirect}
+                type="button" class="fixed bottom-4 right-4 select-none bg-blue-500 hover:bg-blue-700 border-2 text-white text-xl font-bold p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline">
+                <FacebookIcon />
             </button>
-            }
-
         </div>
     );
 };
--- src/Pages/Admin/AdminChat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AdminChat = () => {
-    return (
-        <div>Chat Admin</div>
-    );
-}
-
-export default AdminChat;
