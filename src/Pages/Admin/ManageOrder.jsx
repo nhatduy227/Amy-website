65116002c5f2450b60ab2a0a35c078c4e423ffcc
Nom Phan <np656@drexel.edu>
2023-04-25 18:59:27 -0400

    wip

--- src/Pages/Admin/ManageOrder.jsx
+++ src/Pages/Admin/ManageOrder.jsx
@@ -10,15 +10,38 @@ export default function ManageOrder() {
         const documents = querySnapshot.docs.map(doc => doc.data());
         setOrder(documents);
     };
+    const markOrderAsCompleted = (orderId) => {
+        console.log(orderId)
+    }
+    const deleteOrder = (orderId) => {
+        console.log(orderId)
+    }
     useEffect(() => {
         fetchData();
     }, [order]);
     const OrderCard = ({ order }) => (
-        <div >
-            <h2>{order.orderId}</h2>
-            <p>{order.price}</p>
-            <p>{order.status}</p>
-            <hr />
+        <div class="ml-4 flex flex-1 flex-col">
+            <div class="flex flex-row">
+                <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-primary-default font-semibold">Customer Name</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{order.userInfo.username}</a>
+                    <p class="mt-2 text-gray-500">Order Status: {order.status}</p>
+                    <div class="mt-4">
+                        <span class="text-gray-500 font-bold text-xl">{order.price} VND</span>
+                        {order.status === "completed" ? <button
+                            onClick={() => deleteOrder(order.orderId)}
+                            class="ml-4 px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-full">
+                            Delete
+                        </button> :
+                            <button
+                                onClick={() => markOrderAsCompleted(order.orderId)}
+                                class="ml-4 px-4 py-2 text-sm font-medium text-white bg-primary-default rounded-full">
+                                Mark as Complete
+                            </button>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
     const updatedOrder = order.filter((obj) => obj.status === "pending")
