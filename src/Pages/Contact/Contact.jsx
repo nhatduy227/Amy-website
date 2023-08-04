00a7eb7fc8b552adf6888de840128aac84604b0a
Nom Phan <np656@drexel.edu>
2023-08-04 16:43:39 -0400

    wip

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
+import productBanner from '../../Assets/product-banner.png';
+import la1 from '../../Assets/la1.png';
+import la2 from '../../Assets/la2.png';
+import bubbleDeco from '../../Assets/bubble-deco.png';
 
-import contactBanner from '../../Assets/event-banner.png';
 
 const Contact = () => {
-  // const { t } = useTranslation();
   return (
-    <div className="bg-background-main">
-      {/* Banner */}
-      <div className="">
-        <img src={contactBanner} alt="contact-banner" className="w-full" />
-      </div>
+    <div className="bg-background-main overflow-hidden pb-28">
+      <div className="relative">
+        {/* Decoration */}
+        <img src={la1} alt="" className="hidden lg:block w-[250px] top-[600px] -left-[50px] absolute" />
+        <img
+          src={bubbleDeco}
+          alt="bubbleDeco"
+          className="hidden lg:block w-[400px] top-[800px] -left-[100px] absolute"
+        />
+        <img src={la2} alt="" className="hidden lg:block w-[250px] top-[800px] -right-[50px] absolute" />
+        <img src={la1} alt="" className="hidden lg:block w-[250px] top-[1300px] -left-[50px] absolute" />
+        <img
+          src={bubbleDeco}
+          alt="bubbleDeco"
+          className="hidden lg:block w-[400px] top-[1100px] -right-[120px] absolute"
+        />
+        <img src={la2} alt="" className="hidden lg:block w-[250px] top-[1500px] -right-[50px] absolute" />
 
-      {/* Container */}
-      <div className="py-8 px-36">
+        {/* Banner */}
+        <img className="w-full lg:h-[610px]" src={productBanner} alt="product-banner" />
+
+        {/* Address */}
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex justify-center w-[660px] h-[2px] bg-primary-default my-6" />
+          <h1 className="text-primary-default text-center text-[60px] font-semibold mt-5">LIÊN HỆ</h1>
+        </div>
+
+        {/* Map */}
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-primary-default text-center text-[60px] font-semibold mt-5">LIÊN HỆ</h1>
+        </div>
+
+        {/* Form */}
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-primary-default text-center text-[40px] font-semibold mt-5">Đăng ký dùng thử sản phẩm</h1>
+        </div>
+
+
       </div>
     </div>
   );
